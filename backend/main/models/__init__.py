e92f1de720ead43b7e00154f40429c66e0f17a77
Pichanokk <pichanokk.247@gmai.com>
2025-02-24 00:06:17 +0700

    add : import OTPVerification and Form

--- backend/main/models/__init__.py
+++ backend/main/models/__init__.py
@@ -4,3 +4,5 @@ from .course import Course
 from .subcategory import Subcategory
 from .user import User
 from .enrollment import Enrollment
+from .otp_verification import OTPVerification
+from .form import Form
