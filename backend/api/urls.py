afa30ec8ccc47c6b23294a7b8fe987314d4cafd1
Pichanokk <pichanokk.247@gmai.com>
2025-02-23 23:52:09 +0700

    add path : signup, verify-otp, complete-registration

--- backend/api/urls.py
+++ backend/api/urls.py
@@ -1,6 +1,9 @@
-from main.views import index
+from main.views import *
 from django.urls import path
 
 urlpatterns = [
     path('admin/', index),
+    path('signup/', signup_with_otp, name='signup_with_otp'),
+    path('verify-otp/', verify_otp, name='verify_otp'),
+    path('complete-registration/', complete_registration, name='complete_registration'),
 ]
