963087891b4ebe23c311b65ad811017564ffbed8
Pichanokk <pichanokk.247@gmai.com>
2025-02-24 00:07:08 +0700

    update : email unique=True

--- backend/main/models/user.py
+++ backend/main/models/user.py
@@ -8,7 +8,7 @@ class User(models.Model) :
         INSPECTOR = 'inspector'
     
     user_id = models.UUIDField(primary_key=True, default=uuid.uuid4)
-    email = models.CharField(max_length=200)
+    email = models.CharField(max_length=200, unique=True)
     password = models.CharField(max_length=20)
     name = models.CharField(max_length=200)
     student_code = models.CharField(max_length=10, null=True, blank=True)
